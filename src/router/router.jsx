6281f7116f08b3cd7099b289a8e6809baba7ea78
￵ <21105680+02angeJuice@users.noreply.github.com>
2024-05-03 00:49:57 +0700

    finally

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,13 +1,12 @@
 import React, { Suspense } from 'react'
-import { Navigate, Outlet, useRoutes } from 'react-router-dom'
+import { useRoutes } from 'react-router-dom'
 
 import LoadPage from '../pages/LoadPage'
 import TodoList from '../pages/TodoList'
 
 export default function Router({ setMode }) {
   const routes = useRoutes([
-    { path: '/', element: <Navigate to="/todo" /> },
-    { path: '/todo', element: <TodoList /> },
+    { path: '/', element: <TodoList /> },
     { path: '*', element: <NotFound /> },
   ])
 
